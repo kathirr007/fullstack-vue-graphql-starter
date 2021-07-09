06e03a2100e137bedd35f4247070c9ecb7a14e3c
Kathiravan Kumarasamy <kathiravan.kumarasamy@adelaide.edu.au>
2021-07-09 20:04:46 +0530

    Updated resolvers typeDefs server and user model

--- server.js
+++ server.js
@@ -1,38 +1,36 @@
-const { ApolloServer } = require("apollo-server")
-const mongoose = require('mongoose')
-const fs = require('fs')
-const path = require('path')
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
 
 // Import typeDefs and resolvers
-const filePath = path.join(__dirname, 'typeDefs.gql')
-const typeDefs = fs.readFileSync(filePath, 'utf-8')
+const filePath = path.join(__dirname, "typeDefs.gql");
+const typeDefs = fs.readFileSync(filePath, "utf-8");
 
 // Import Environment variables and mongoose models
-require('dotenv').config({path: 'variables.env'})
-const User = require('./models/user')
-const Post = require('./models/post')
-const resolvers = require('./resolvers')
+require("dotenv").config({ path: "variables.env" });
+const User = require("./models/user");
+const Post = require("./models/post");
+const resolvers = require("./resolvers");
 
 // Connect to MongoDB Database
 mongoose
-  .connect(
-      process.env.MONGO_URI,
-      { 
-        useCreateIndex: true,
-        useNewUrlParser: true 
-      }
-    )
-  .then(() => console.log('DB Connected...'))
-  .catch(err => console.error(err))
+  .connect(process.env.MONGO_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected..."))
+  .catch((err) => console.error(err));
 
-  // Create Appollo/GraphQL Server using typeDefs, resolvers and context Object
+// Create Appollo/GraphQL Server using typeDefs, resolvers and context Object
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: {
     User,
-    Post
-  }
+    Post,
+  },
 });
 
 server.listen().then(({ url }) => {
